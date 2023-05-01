284e03ffd71cb32b5b998938f6d79ad85112bd2f
Manasi Pawar <118588422+ManasiPawar263@users.noreply.github.com>
2023-05-01 15:44:13 +0530

    Add files via upload

---
+++ DDL_sql.sql
@@ -0,0 +1,14 @@
+# create new database
+create database excelr;
+
+# See database created
+show databases;
+
+# Use the database as default schema
+use excelr;
+
+# create table with column as id, name and age
+create table personal_info(
+id int,
+emp_name char(20),
+age int);
